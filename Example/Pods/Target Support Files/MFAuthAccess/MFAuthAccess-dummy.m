f7e66c2ac1441f831f0ec650198c8a2fb598f90a
akashbelekar2428 <akashbelekar108@gmail.com>
2023-05-05 11:25:35 +0530

    setup Auth

---
+++ Example/Pods/Target Support Files/MFAuthAccess/MFAuthAccess-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MFAuthAccess : NSObject
+@end
+@implementation PodsDummy_MFAuthAccess
+@end
---
+++ Example/Pods/Target Support Files/Pods-MFAuthAccess_Example/Pods-MFAuthAccess_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MFAuthAccess_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MFAuthAccess_Example
+@end
